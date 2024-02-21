dc0137715a9c6d5882d0c85dac3d1f32ed066a06
Yorlean <159148143+Yorlean@users.noreply.github.com>
2024-02-21 09:38:57 -0500

    Add files via upload

---
+++ Listas/Listas.py
@@ -0,0 +1,17 @@
+#Ejercicio_Listas
+valor_a = 5
+#              0  1  2  3  4   5   6   7   8
+valor_lista = [4, 7, 8, 9, 10, 20, 30, 40, 55]
+print (valor_lista[8])
+#Ejercicios_Listas_Texto
+lista_2 = ["Saludos", "Juan", "Estudiante", "Incap"]
+print (lista_2[1])
+#Ejercicios_Listas_Aplicando_Append
+lista_2.append ("Prueba")
+print (lista_2)
+#Ejercicios_Listas_Aplicando_Pop
+lista_2.pop (4)
+print (lista_2)
+#Ejercicios_Listas_Aplicando_Pop
+lista_2.clear ()
+print (lista_2)
