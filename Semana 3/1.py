04a0a9b37471d2ed4a4291b0846d22f73e9c1616
Yorlean <159148143+Yorlean@users.noreply.github.com>
2024-04-25 06:56:36 -0500

    Delete Semana 3 directory

--- Semana 3/1.py
+++
@@ -1,7 +0,0 @@
-metros_a = input("Ingrese un valor en metros:")
-metros = float (metros_a)
-pies = metros * 3.28084
-pulgadas = pies * 12
-
-print ("El valor en pies es:", pies)
-print ("el valor en pulgadas es:", pulgadas)
